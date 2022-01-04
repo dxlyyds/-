96467b54d5574b5aafcdf30112d0cbab4fdde44a
dxlyyds <1424737567@qq.com>
2022-01-05 01:18:19 +0800

    图书管理系统

---
+++ vue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<el-config-provider :locale="locale">
+  <div>
+    <!--* 头部-->
+    <Header></Header>
+    <!--主体-->
+    <div style ="display: flex">
+      <!--侧边栏-->
+      <Aside></Aside>
+      <!--内容-->
+      <router-view style="flex: 1"/>
+    </div>
+  </div>
+</el-config-provider>
+</template>
+<script>
+import { ElConfigProvider } from "element-plus"
+import zhCn from "element-plus/lib/locale/lang/zh-cn"
+import Header from './components/Header.vue';
+import Aside from './components/Aside.vue';
+export default{
+  name:"Layout",
+  components:{
+    Header,
+    Aside,
+    [ElConfigProvider.name]:ElConfigProvider,
+  },
+  data(){
+    return{
+      locale:zhCn
+    }
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ vue/src/components/Aside.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <el-menu
+        style="width:200px; min-height: calc(100vh - 50px);"
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>选项1</span>
+          </template>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>选项2</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>选项3</span>
+        </el-menu-item>
+    </el-menu>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
